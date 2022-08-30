ebbc26b2d327427e6b0969a9cb32d83b783194f0
Your Name <you@example.com>
2022-08-30 11:17:38 -0500

    responses

--- backend/app/Http/Controllers/bootcampController.php
+++ backend/app/Http/Controllers/bootcampController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Bootcamp;
+
 
 class bootcampController extends Controller
 {
@@ -13,7 +15,8 @@ class bootcampController extends Controller
      */
     public function index()
     {
-        echo "aqui se va a mostrar todos los bootcamp";
+        //echo "aqui se va a mostrar todos los bootcamp";
+        return response()->json(["sucess" => true,"datos" => Bootcamp::all()] , 200);
     }
 
     /**
@@ -24,7 +27,15 @@ class bootcampController extends Controller
      */
     public function store(Request $request)
     {
-        echo "aqui se crea un bootcamp";
+        //verificar que llego aqui el payload
+        $b=Bootcamp::create([
+            "name" => $request->name,
+            "description" => $request->description,
+            "website" => $request->website,
+            "phone" => $request->phone,
+            "user_id" => $request->user_id
+        ]);
+        return response([ "success" => true , "data" => $b] , 201 );
     }
 
     /**
@@ -35,7 +46,7 @@ class bootcampController extends Controller
      */
     public function show($id)
     {
-        echo "mostrar un bootcamp cuyo id es: $id";
+        return response()->json(["success" => true , "data" => Bootcamp::find($id)] , 200);
     }
 
     /**
@@ -47,7 +58,14 @@ class bootcampController extends Controller
      */
     public function update(Request $request, $id)
     {
-        echo "aqui se va a actualizar el bootcamp con id $id";
+        //1. seleccionar el bootcamp por id
+        $bootcamp = Bootcamp::find($id);
+        //2. Actualizarlo
+        $bootcamp->update(
+            $request->all()
+        );
+        //3. Hacer el Response respectivo
+        return response()->json(["success" => true , "data" => $bootcamp], 200);
     }
 
     /**
@@ -58,6 +76,12 @@ class bootcampController extends Controller
      */
     public function destroy($id)
     {
-        echo "aqui se va a eliminar el bootcamp cuyo id es $id";
+        //echo "aqui se va a eliminar el bootcamp cuyo id es $id";
+        //1. seleccionas el bootcamp
+        $bootcamp = Bootcamp::find($id);
+        //2. eliminar ese bootcamp
+        $bootcamp->delete();
+        //3. Response:
+        return response()->json(["success" => true ,"message" => "bootcamp eliminado", "data" => $bootcamp->id ], 200);
     }
 }
---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use File;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //1.Leer el archivo de datos
+        $json=File::get('database/_data/user.json');
+        //2.convertir los datos en un arreglo
+        $arreglo_usuarios=json_decode($json);
+        //3.recorrer el arreglo
+        //var_dump($arreglo_usuarios);
+        foreach($arreglo_usuarios as $usuario){
+            //4.registrar el usuario en base de datos
+            //  se utiliza el metodo ::create
+            user::create([
+                "username" => $usuario->name,
+                "email" => $usuario->email,
+                "password" => Hash::make($usuario->password),
+            ]);
+        }
+        
+
+    }
+}
